6544a059f8b0592a5fab2521bdacb99897dab106
Pavle Djurdjic <Pavle Djurdjic@MANGORO>
2023-07-22 16:25:02 +0200

    First working solution in cpp

--- sha256-cpp/SHA256.h
+++ sha256-cpp/SHA256.h
@@ -1,12 +1,16 @@
 #pragma once
-#include <stdint.h>
-#include <iostream>
-#include <sstream>
-#include <bitset>
+#include <string>
 #include <vector>
-#include <iomanip>
-#include <cstring>
 using namespace std;
+
+const int a = 0;
+const int b = 1;
+const int c = 2;
+const int d = 3;
+const int e = 4;
+const int f = 5;
+const int g = 6;
+const int h = 7;
 const uint32_t K[64] = {
 		1116352408, 1899447441, 3049323471, 3921009573, 961987163, 1508970993, 2453635748, 2870763221,
 		3624381080, 310598401, 607225278, 1426881987, 1925078388, 2162078206, 2614888103, 3248222580,
@@ -17,141 +21,102 @@ const uint32_t K[64] = {
 		430227734, 506948616, 659060556, 883997877, 958139571, 1322822218, 1537002063, 1747873779,
 		1955562222, 2024104815, 2227730452, 2361852424, 2428436474, 2756734187, 3204031479, 3329325298
 };
-const uint32_t H[8] = { 0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19 };
+uint32_t H[8] = { 0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19 };
+
 uint32_t right_rotate(uint32_t x, uint32_t n) {
 	return (x >> n) | (x << (32 - n));
 }
+
 uint32_t little_sigma_0(uint32_t x) {
 	return right_rotate(x, 7) ^ right_rotate(x, 18) ^ (x >> 3);
 }
+
 uint32_t little_sigma_1(uint32_t x) {
 	return right_rotate(x, 17) ^ right_rotate(x, 19) ^ (x >> 10);
 }
+
 uint32_t big_sigma_0(uint32_t x) {
 	return right_rotate(x, 2) ^ right_rotate(x, 13) ^ right_rotate(x, 22);
 }
+
 uint32_t big_sigma_1(uint32_t x) {
 	return right_rotate(x, 6) ^ right_rotate(x, 11) ^ right_rotate(x, 25);
 }
-void message_schedule(uint32_t(&W)[64], char(&A)[64]) {
+
+void message_schedule(uint32_t(&W)[64], uint8_t(&block_64_bit)[64]) {
 	for (int i = 0; i < 16; i++) {
-		W[i] = (A[i * 4] << 24) |
-			(A[i * 4 + 1] << 16) |
-			(A[i * 4 + 2] << 8) |
-			(A[i * 4 + 3]);
+		W[i] =  ( block_64_bit [i * 4]  <<  24)     |
+				( block_64_bit [i * 4 + 1]  <<  16) |
+				( block_64_bit [i * 4 + 2]  <<  8)  |
+				( block_64_bit [i * 4 + 3]);
 	}
+
 	for (int i = 16; i < 64; i++) {
-		W[i] = little_sigma_1(W[i - 2]) + W[i - 7] +
-			little_sigma_0(W[i - 15]) + W[i - 16];
+		W[i] =	little_sigma_1( W [i - 2] )  +  W[i - 7]  +
+				little_sigma_0( W [i - 15] )  +  W[i - 16];
 	}
 }
+
 uint32_t choice(uint32_t x, uint32_t y, uint32_t z) {
 	return (x & y) ^ (~x & z);
 }
+
 uint32_t majority(uint32_t x, uint32_t y, uint32_t z) {
 	return (x & y) ^ (x & z) ^ (y & z);
 }
-void round(uint32_t(&state)[8], uint32_t round_constant, uint32_t schedule_word) {
-	uint32_t T1 = state[7] +
-		big_sigma_1(state[4]) +
-		choice(state[4], state[5], state[6]) +
-		round_constant +
-		schedule_word;
-	uint32_t T2 = big_sigma_0(state[0]) +
-		majority(state[0], state[1], state[2]);
+
+void round(uint32_t(&H)[8], uint32_t round_constant, uint32_t schedule_word) {
+	uint32_t T1 =	H[h] +
+					big_sigma_1(H[e]) +
+					choice(H[e], H[f], H[g]) +
+					round_constant +
+					schedule_word;
+
+	uint32_t T2 =	big_sigma_0(H[a]) +
+					majority(H[a], H[b], H[c]);
+
 	for (int i = 7; i > 0; i--) {
-		state[i] = state[i - 1];
+		H[i] = H[i - 1];
 	}
-	state[0] = T1 + T2;
-	state[4] += T1;
+
+	H[a] = T1 + T2;
+	H[e] += T1;
 }
-void compress_block(uint32_t(&initial_state)[8], char(&block)[64]) {
+void compress_block(uint32_t(&H)[8], uint8_t(&block_64_bit)[64]) {
 	uint32_t W[64];
-	uint32_t state[8];
-	for (int i = 0; i < 8; i++) state[i] = initial_state[i];
-	message_schedule(W, block);
-	for (int i = 0; i < 64; i++) round(state, W[i], K[i]);
-	for (int i = 0; i < 8; i++) initial_state[i] += state[i];
-}
-void to_bitset(vector<unsigned char> &block, string message) {
-	for (int i = 0; i < message.size(); i++) {
-		bitset<8> b = message[i];
-		block.push_back(b.to_ulong());
-	}
-}
-void pad_5122(vector<unsigned char> &block) {
-	unsigned char initial_byte = 128;
-	uint64_t l = block.size() * 8; // Message size as 64bit number
-	int x = 512;
-	while ((x < l)) {
-		x *= 2;
-	}
-	int k = (448 - 8) - l; // l + 1 + k = 448 mod 512
+	uint32_t h[8];
 
-	block.push_back(initial_byte);
+	message_schedule(W, block_64_bit);
 
-	// Pad the rest of the bits with 0 (up until last 64bits)
-	for (int i = 1; i <= k; i += 8) {
-		block.push_back(0);
-	}
-
-	// Add last 64bits that represent message size
-	for (int i = 7; i >= 0; i--) {
-		block.push_back((l >> (8 * i))& (0xff));
-	}
+	for (int i = 0; i < 8; i++) h[i] = H[i];
+	for (int i = 0; i < 64; i++) round(h, W[i], K[i]);
+	for (int i = 0; i < 8; i++) H[i] += h[i];
 }
 
-void resize_block(uint32_t* block, vector<unsigned char> &input) {
-	for (int i = 0; i < 16; i++) {
-		block[i] =	(input[i * 4] << 24) |
-					(input[i * 4 + 1] << 16) |
-					(input[i * 4 + 2] << 8) |
-					(input[i * 4 + 3]);
-	}
-}
-std::string vectorToBinaryString(const std::vector<unsigned char>& vec) {
-	std::string binaryString;
-	for (const unsigned char& value : vec) {
-		std::bitset<8> bits(value); // Convert the unsigned char value to binary (8 bits)
-		binaryString += bits.to_string(); // Append the binary representation to the string
-	}
-	return binaryString;
-}
-
-void pad_512(vector<unsigned char> message) {
-	/*cout << "Message=" << message << endl;
-	cout << "Length=" << length;*/
-	cout << "Message Size = " << message.size() * 8 << endl;
-	cout << "Message" << endl;
-	for (int i = 0; i < message.size(); i++) {
-		cout << message.at(i);
-	}
-	cout << endl;
-
+void sha256(string m) {
+	vector<uint8_t> message(m.begin(), m.end());
 	int N = 1;
-	int size = message.size();
-	int l = (size * 8);
+	int l = (message.size() * 8);
 	message.push_back(128);
 
-	if (l + 64 > 512) {
-		N += (l / 512);
+	if ((l + 64) > 512) {
+		N += ((l + 64) / 512);
 	}
 	for (int i = l; i < ((N * 512) - 8 - 64); i += 8) {
 		message.push_back(0);
 	}
 	for (int i = 0; i < 8; ++i) {
-		message.push_back((unsigned char)((((unsigned long long) l) >> (56 - (8 * i))) & 0xFFu));
+		message.push_back((uint8_t)((((unsigned long long) l) >> (56 - (8 * i))) & 0xFFu));
 	}
 
-	string res = vectorToBinaryString(message);
-	std::cout << "Binary representation " << endl << res << std::endl;
-	cout << endl << "Total Bits Size = " << res.size();
+	uint8_t block[64];
+	for (int i = 0; i < N; i++) {
+		std::copy(reinterpret_cast<uint8_t*>(&message[i * 64]),
+			reinterpret_cast<uint8_t*>(&message[(i * 64) + 63]),
+			block);
 
-	
-}
-
-void sha256(string message) {
-	vector<unsigned char> block(message.begin(), message.end());
-	pad_512(block);
+		block[63] = message[(i * 64) + 63];
+		compress_block(H, block);
+	}
 }
 
--- sha256-cpp/sha256-cpp.cpp
+++ sha256-cpp/sha256-cpp.cpp
@@ -1,59 +1,18 @@
 // sha256-cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-#include <stdint.h>
-#include <iostream>
-#include <sstream>
-#include <bitset>
-#include <vector>
-#include <iomanip>
-#include <cstring>
 #include "SHA256.h"
+#include "Utils.h"
 using namespace std;
-
-unsigned int K1[64] = {
-		0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,
-		0x923f82a4,0xab1c5ed5,0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,
-		0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,0xe49b69c1,0xefbe4786,
-		0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-		0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,
-		0x06ca6351,0x14292967,0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,
-		0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,0xa2bfe8a1,0xa81a664b,
-		0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-		0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,
-		0x5b9cca4f,0x682e6ff3,0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,
-		0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
-};
-unsigned int static H0 = 0x6a09e667;
-unsigned int static H1 = 0xbb67ae85;
-unsigned int static H2 = 0x3c6ef372;
-unsigned int static H3 = 0xa54ff53a;
-unsigned int static H4 = 0x510e527f;
-unsigned int static H5 = 0x9b05688c;
-unsigned int static H6 = 0x1f83d9ab;
-unsigned int static H7 = 0x5be0cd19;
 
 int main()
 {
-	/*cout << "Output = ";
-	string str = "To calculate the message length of 2^64, we first need to determine the number of bits required to represent this number. 2^64 is a very large number, and it will have 65 digits if we write it out as 18,446,744,073,709,551,616.";
-	vector<unsigned char> block;
-	uint32_t init_block[16];
+	string s1 = "Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+	string s2 = "shashavo";
+	sha256(s1);
 
-	to_bitset(block, str);
-
-	for (int i = 0; i < block.size(); i++) {
-		cout << static_cast<int>(block[i]) << " ";
-	}
-	pad_512(block);
-	resize_block(init_block, block);
-
-	cout << endl << "Resized block = ";
-	for (int i = 0; i < 16; i++) {
-		cout << init_block[i] << " ";
-	}*/
-	string s = "Lorem psum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-	sha256(s);
+	display_final_state_H_as_uint();
+	display_final_state_H_as_bits();
+	display_final_state_as_hex();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
